acad930e0aaeb6f42f6903b36c98a6fedc510cad
Alireza Asadi <asadi.alireza@yahoo.com>
2016-02-03 17:38:06 +1100

    Create server.js

---
+++ server.js
@@ -0,0 +1,79 @@
+# websocket-sample
+
+/*
+ *********************************************************************
+ ************************ HTTP server **************************
+ *********************************************************************
+ */
+
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+
+var dispatcher = require('httpdispatcher');
+//Lets use our dispatcher
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStaticDirname(__dirname);
+dispatcher.setStatic('');
+
+
+//A sample GET request    
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});    
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
+
+
+
+
+
+/*
+ *********************************************************************
+ ************************ Web socket server **************************
+ *********************************************************************
+ */
+
+
+var ws = require("nodejs-websocket")
+
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+}).listen(8001)
